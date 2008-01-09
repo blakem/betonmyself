8b71e94320f92d4330a46a683e77451065ed6570
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-09 08:21:48 +0000

    Tweak listing of columns....
    git-svn-id: svn://www.blakem.com/betonmyself@24 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,9 +1,12 @@
 class MembersController < ApplicationController
   active_scaffold :bet do |config|
     config.label = "Your Motivational Bets"
-#    config.columns = [:name, :phone, :company_type, :comments]
-#    list.columns.exclude :comments
-#    list.sorting = {:name => 'ASC'}
+    config.columns = [:descr, :amount, :due_date, :notes]
+    list.sorting = {:descr => 'ASC'}
     columns[:descr].label = "Goal"
+    columns[:amount].label = "Payoff"
+  end
+  def conditions_for_collection
+#    ['amount IN (?)', ['34']]
   end
 end
