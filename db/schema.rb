2e78540d2ea106ceaca0f09d1271e6114fe2f07e
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-09 10:18:32 +0000

    Added new signup code
    git-svn-id: svn://www.blakem.com/betonmyself@25 9161c798-24a2-4987-b6fa-c600ba363c56

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 3) do
 
   create_table "bets", :force => true do |t|
     t.string   "descr"
@@ -21,11 +21,14 @@ ActiveRecord::Schema.define(:version => 2) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username"
-    t.string   "password"
-    t.string   "name"
+    t.string   "login"
+    t.string   "email"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
   end
 
 end
