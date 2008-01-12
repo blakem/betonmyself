68bd3f5bc259d97b09c89859b4e1a1b06dbd8241
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-12 11:50:49 +0000

    Add price checking on wagers...
    git-svn-id: svn://www.blakem.com/betonmyself@54 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,3 +1,9 @@
 class Bet < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :user_id, :state, :descr, :price, :due_date
+  def validate
+    if price > self.user.ballance
+      errors.add(:price, "is more than your ballance") 
+    end
+  end
 end
