ce8ecaf3bd79871720ff4644ba15012e7175109d
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-12 10:18:21 +0000

    Secured site with ssl
    git-svn-id: svn://www.blakem.com/betonmyself@47 9161c798-24a2-4987-b6fa-c600ba363c56

--- sysconf/bin/start_mongrel.sh
+++ sysconf/bin/start_mongrel.sh
@@ -1,2 +1,10 @@
-(cd $root; mongrel_rails start -d -p 8000 -e development -P $root/tmp/pids/mongrel.pid -a 127.0.0.1)
+if [ "$1" = "" ]; then
+  environment='development'
+else
+  environment=$1
+fi
+if [ -f "$root/tmp/pids/mongrel.pid" ]; then
+  (cd $root; sysconf/bin/stop_mongrel.sh)
+fi
+(cd $root; mongrel_rails start -d -p 8000 -e $environment -P $root/tmp/pids/mongrel.pid -a 127.0.0.1)
 
