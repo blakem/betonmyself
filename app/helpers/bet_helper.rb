5ec2a0ef094cb8c9fa688b1e904e85dec1426e21
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-12 12:36:59 +0000

    Really crappy way to give a default value to price
    git-svn-id: svn://www.blakem.com/betonmyself@56 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/helpers/bet_helper.rb
+++ app/helpers/bet_helper.rb
@@ -1,2 +1,6 @@
 module BetHelper
+   def price_form_column(record, input_name)
+     eid = params[:eid]
+     return '<input autocomplete="off" class="price-input text-input" id="record_price_' + eid + '" name="record[price]" size="20" type="text" value="5">'
+   end
 end
