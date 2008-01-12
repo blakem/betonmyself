e021bb378c5c8d7a512b5cfa447028d08a01e589
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-12 10:33:43 +0000

    Make stop_mongrel nicer if it can't find a pid
    git-svn-id: svn://www.blakem.com/betonmyself@50 9161c798-24a2-4987-b6fa-c600ba363c56

--- sysconf/bin/stop_mongrel.sh
+++ sysconf/bin/stop_mongrel.sh
@@ -1,2 +1,6 @@
-(cd $root; mongrel_rails stop -P $root/tmp/pids/mongrel.pid)
+if [ -f "$root/tmp/pids/mongrel.pid" ]; then
+  (cd $root; mongrel_rails stop -P $root/tmp/pids/mongrel.pid)
+else
+  echo "Can't find mongrel pid: $root/tmp/pids/mongrel.pid"
+fi
 
