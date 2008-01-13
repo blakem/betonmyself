626aec3c03a7584d47ffa4b4471d59e477c74ba0
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-13 23:57:27 +0000

    Constantize Minimum and Default bet amounts
    git-svn-id: svn://www.blakem.com/betonmyself@60 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/helpers/bet_helper.rb
+++ app/helpers/bet_helper.rb
@@ -1,6 +1,10 @@
 module BetHelper
    def price_form_column(record, input_name)
      eid = params[:eid]
-     return '<input autocomplete="off" class="price-input text-input" id="record_price_' + eid + '" name="record[price]" size="20" type="text" value="5">'
+     return '<input autocomplete="off" class="price-input text-input" ' +
+            'id="record_price_' + eid + '" name="record[price]" size="20" ' +
+            'type="text" value="' + 
+            money_format(BomConstant::DEFAULT_BET) +
+            '">'
    end
 end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,13 +1,16 @@
 class Bet < ActiveRecord::Base
+  include BomUtility
   belongs_to :user
   validates_presence_of :user_id, :state, :descr, :price, :due_date
   def validate
-    if not price.nil? 
+    if not price.nil?
       if price > self.user.ballance
         errors.add(:price, "is more than you have in your incentive bank.")
       end
-      if price < 500
-        errors.add(:price, "is too low. Minimum is $5.")
+      if price < BomConstant::MINIMUM_BET
+        errors.add(:price, "of $" + money_format(price) + 
+                   " is too low. Minimum is $" + 
+                   money_format(BomConstant::MINIMUM_BET) + ".")
       end
       if errors.count > 0
         self.price /= 100
