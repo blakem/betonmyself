e8dd496be9345be0863eff90bd4219db91ca4414
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-15 07:11:59 +0000

    Make the Notes TextArea look like the rest of the form...
    git-svn-id: svn://www.blakem.com/betonmyself@82 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/helpers/bet_helper.rb
+++ app/helpers/bet_helper.rb
@@ -10,8 +10,11 @@ module BetHelper
       active_scaffold_input_text_options(:name => input_name, :value => value)
   end
   def notes_form_column(record, input_name)
-    text_area :record, :notes, 
-      active_scaffold_input_text_options(:name => input_name, :cols => 80)
+    text_area :record, :notes, {
+      :cols => 40,
+      :style => 'letter-spacing: -1px;border: solid 1px #1F7F00;' +
+                'font: bold 16px arial'
+    }
   end
   def due_date_form_column(record, input_name) 
     date_select :record, :due_date, 
