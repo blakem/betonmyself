8be8db9d781d64cecbadf3ebafaee7c509b8fcb0
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-17 08:28:34 +0000

    Added lots of buttons to the menu bar... Starting look pretty slick!
    git-svn-id: svn://www.blakem.com/betonmyself@108 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,18 +12,75 @@ module ApplicationHelper
   def main_menu
     render :file => "#{RAILS_ROOT}/app/views/menu_items/_menubar.rhtml", :use_full_path => false, :locals => {:level => 0, :depth => 0, :class_attr => nil, :menu_data => self.menu_data }
   end
-  def menu_data 
-    return [
-      {   
-        'text' => 'Home',
+  def menu_data
+    buttons = {
+      'welcome' => {
+        'text' => 'Welcome',
         'link' => '/',
         'selected' => 1
       },
-      {
+      'members' => {
         'text' => 'Members',
         'link' => '/members',
         'selected' => 0
-      }
-    ]
+      },
+      'play' => {
+        'text' => 'Play!',
+        'link' => '/members',
+        'selected' => 0
+      },
+      'signup' => {
+        'text' => 'SignUp',
+        'link' => '/signup',
+        'selected' => 0
+      },
+      'help' => {
+        'text' => 'Help',
+        'link' => '/help',
+        'selected' => 0
+      },
+      'support' => {
+        'text' => 'Support',
+        'link' => '/support',
+        'selected' => 0
+      },
+      'purchase' => {
+        'text' => 'Add Money',
+        'link' => '/purchase',
+        'selected' => 0
+      },
+      'donate' => {
+        'text' => 'Donate',
+        'link' => '/',
+        'selected' => 0
+      },
+      'logout' => {
+        'text' => 'Log Out',
+        'link' => '/logout',
+        'selected' => 0,
+      },
+      'cashout' => {
+        'text' => 'Cash Out',
+        'link' => '/cash_out',
+        'selected' => 0,
+      },
+    };
+    if not logged_in?
+      [
+       buttons['welcome'],
+       buttons['signup'],
+       buttons['members'],
+      ]
+    else
+      [
+       buttons['play'],
+       buttons['purchase'],
+       buttons['cashout'],
+       buttons['help'],
+       buttons['support'],
+       buttons['donate'],
+       buttons['logout'],
+      ]
+    end
   end
 end
