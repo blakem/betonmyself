9e3034b6ccb6f2a714c559f5ffc842dc8f0e299c
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-01-26 14:10:46 +0000

    Working (barely) account management page
    git-svn-id: svn://www.blakem.com/betonmyself@145 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/controllers/update_account_controller.rb
+++ app/controllers/update_account_controller.rb
@@ -6,17 +6,14 @@ class UpdateAccountController < ApplicationController
   def change
     @selected_button = 'support'
     @user = self.current_user
-    next_page = '/members'
     new_fields = params[:user]
-    if (new_fields[:password] != new_fields[:password_confirm]) 
-      next_page = '/update_account'
-    else
-      @user.first_name = new_fields[:first_name]
-      @user.last_name = new_fields[:last_name]
-      @user.email = new_fields[:email]
-      @user.password = new_fields[:password]
-      @user.save!
-    end
-    redirect_to(next_page)
+    @user.first_name = new_fields[:first_name]
+    @user.last_name = new_fields[:last_name]
+    @user.email = new_fields[:email]
+    @user.password = new_fields[:password]
+    @user.save!
+    redirect_to('/members')
+  rescue ActiveRecord::RecordInvalid
+    render :action => 'index'
   end
 end
