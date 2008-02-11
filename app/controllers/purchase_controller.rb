5132359b383ab6e45d94b57010d7f8d7dc399260
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-02-11 10:17:05 +0000

    Prettier (though still not working.... credit card form)
    git-svn-id: svn://www.blakem.com/betonmyself@244 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -17,6 +17,8 @@ class PurchaseController < ApplicationController
   def form
     @selected_button = 'purchase'
     @amount = params[:transaction][:price]
+    @address = BomAddress.new
+    @states = get_states
   end
   
   # Use the DirectPayment API
@@ -150,5 +152,60 @@ billing_address = {
     def load_card
       @creditcard = ActiveMerchant::Billing::CreditCard.new(params[:creditcard])
     end
-  
+
+    def get_states
+      [
+       ["Alabama", "AL"],
+       ["Alaska", "AK"],
+       ["Arizona", "AZ"],
+       ["Arkansas", "AR"],
+       ["California", "CA"],
+       ["Colorado", "CO"],
+       ["Connecticut", "CT"],
+       ["Delaware", "DE"],
+       ["District Of Columbia", "DC"],
+       ["Florida", "FL"],
+       ["Georgia", "GA"],
+       ["Hawaii", "HI"],
+       ["Idaho", "ID"],
+       ["Illinois", "IL"],
+       ["Indiana", "IN"],
+       ["Iowa", "IA"],
+       ["Kansas", "KS"],
+       ["Kentucky", "KY"],
+       ["Louisiana", "LA"],
+       ["Maine", "ME"],
+       ["Maryland", "MD"],
+       ["Massachusetts", "MA"],
+       ["Michigan", "MI"],
+       ["Minnesota", "MN"],
+       ["Mississippi", "MS"],
+       ["Missouri", "MO"],
+       ["Montana", "MT"],
+       ["Nebraska", "NE"],
+       ["Nevada", "NV"],
+       ["New Hampshire", "NH"],
+       ["New Jersey", "NJ"],
+       ["New Mexico", "NM"],
+       ["New York", "NY"],
+       ["North Carolina", "NC"],
+       ["North Dakota", "ND"],
+       ["Ohio", "OH"],
+       ["Oklahoma", "OK"],
+       ["Oregon", "OR"],
+       ["Pennsylvania", "PA"],
+       ["Rhode Island", "RI"],
+       ["South Carolina", "SC"],
+       ["South Dakota", "SD"],
+       ["Tennessee", "TN"],
+       ["Texas", "TX"],
+       ["Utah", "UT"],
+       ["Vermont", "VT"],
+       ["Virginia", "VA"],
+       ["Washington", "WA"],
+       ["West Virginia", "WV"],
+       ["Wisconsin", "WI"],
+       ["Wyoming", "WY"]
+      ]
+    end
 end
---
+++ app/models/bom_address.rb
@@ -0,0 +1,3 @@
+class BomAddress
+  attr_accessor :address1, :address2, :city, :state, :zip
+end
