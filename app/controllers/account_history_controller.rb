1caab212f10edbd3737e7f0a81e2f9f65a3ac7fa
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-02-14 09:47:02 +0000

    Limit the account_history controller to members only.
    git-svn-id: svn://www.blakem.com/betonmyself@271 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/controllers/account_history_controller.rb
+++ app/controllers/account_history_controller.rb
@@ -1,5 +1,8 @@
 class AccountHistoryController < ApplicationController
   layout "members"
+  def authorized?
+    members_authorized?
+  end
   def index
     @selected_button = 'support'
     @user = current_user
