36130a721067024c8b154189889d43b412f09060
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-02-21 04:18:25 +0000

    Apache config file for development listner
    git-svn-id: svn://www.blakem.com/betonmyself@324 9161c798-24a2-4987-b6fa-c600ba363c56

--- sysconf/bin/start_mongrel.sh
+++ sysconf/bin/start_mongrel.sh
@@ -1,11 +1,14 @@
 if [ "$1" = "" ]; then
   environment='development'
+  port=8080
 else
   environment=$1
+  port=8000
 fi
+
 if [ -f "$root/tmp/pids/mongrel.pid" ]; then
   (cd $root; sysconf/bin/stop_mongrel.sh)
   sleep 3;
 fi
-(cd $root; mongrel_rails start -d -p 8000 -e $environment -P $root/tmp/pids/mongrel.pid -a 127.0.0.1)
+(cd $root; mongrel_rails start -d -p $port -e $environment -P $root/tmp/pids/mongrel.pid -a 127.0.0.1)
 
