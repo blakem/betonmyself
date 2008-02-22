cd778e18ec5996a9ad9238c1bf3a4ef64fd32f3d
root <root@9161c798-24a2-4987-b6fa-c600ba363c56>
2008-02-22 06:39:15 +0000

    Don't force active login on admin
    git-svn-id: svn://www.blakem.com/betonmyself@334 9161c798-24a2-4987-b6fa-c600ba363c56

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,5 @@
 class AdminController < ApplicationController
   layout "members"
-  def authorized?
-    members_authorized? and current_user.is_admin
-  end
   def index
     @selected_button = 'admin'
   end
